4b071131e4a1f3dd51d38825f0983902c940bfb4
siva govind <jayandrasivagovind@gmail.com>
2025-08-24 10:30:14 +0530

    feat: Final working version with all fixes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors"); // 1. UNCOMMENT THIS LINE
 
 const app = express();
+
+app.use(cors()); // 2. ADD THIS LINE
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -28,10 +30,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Test application." });
 });
 
-require("./app/routes/turorial.routes")(app);
+require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
